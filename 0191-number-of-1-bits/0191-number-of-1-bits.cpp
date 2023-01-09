423dab6703a17c7f72cc4f4384ddba0858f9a309
R Abinaya <105599764+r-abinaya@users.noreply.github.com>
2023-01-09 19:41:41 +0530

    Time: 3 ms (58.68%), Space: 6.1 MB (20.91%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,9 +3,11 @@ public:
     int hammingWeight(uint32_t n) {
         int count=0;
         while(n){
-             n&=n-1;
+             n&=n-1;          //n-1 makes the bits 1 after the first bit 1 from right
             count++;
         }
         return count;
     }
 };
+
+
