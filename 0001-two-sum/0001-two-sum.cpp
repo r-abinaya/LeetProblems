5af691219b4e0aebc1d96f2ef970091d32d65cbe
R Abinaya <105599764+r-abinaya@users.noreply.github.com>
2023-01-09 18:46:27 +0530

    Time: 8 ms (96.97%), Space: 10.8 MB (42.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -8,12 +8,13 @@ public:
         int n=nums.size();
         for(int i=0;i<n;i++)
         {
+          
                 // if(count.find(target-nums[i])!=count.end())   
             if(count.count(target-nums[i]))
             {
                 return {count[target-nums[i]],i};
             }
-            count[nums[i]]=i;
+              count[nums[i]]=i;
         }
         return {};
     }
