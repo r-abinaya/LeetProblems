e57ec0fdb00e02d7f2bf2e8a8771da87194b82c9
R Abinaya <105599764+r-abinaya@users.noreply.github.com>
2023-02-12 10:19:15 +0530

    Time: 3 ms (43.45%), Space: 6 MB (64.59%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-     int trigger = a & b;
-        int ret = a ^ b;
-        while(trigger)
-        {
-            int temp = (trigger & 0xffffffff)<<1; //not a problem anymore!
-            trigger = temp & ret;
-            ret ^= temp;
-        }
-        return ret;
+        uint carry = a & b;
+        uint add = a ^ b;
+
+        return (carry) ? getSum(add,carry<<1) : add;
+
     }
 };
