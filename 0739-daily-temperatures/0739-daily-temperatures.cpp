e590e2b8180fd90250bb27911bb47c5417a10b37
R Abinaya <105599764+r-abinaya@users.noreply.github.com>
2023-02-19 10:34:56 +0530

    Time: 177 ms (96.95%), Space: 98.6 MB (80.19%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
        vector<int> res(temperatures.size());
-       for (int i = temperatures.size() - 1; i >= 0; --i) {
-        int j = i+1;
-        while (j < temperatures.size() && temperatures[j] <= temperatures[i]) {
-            if (res[j] > 0) j = res[j] + j;
-            else j = temperatures.size();
-        }
-        // either j == size || temperatures[j] > temperatures[i]
-        if (j < temperatures.size()) res[i] = j - i;
-    }
-    return res;   
+       for(int i=temperatures.size()-1;i>=0;i--)
+       {
+           int j=i+1;
+           while(j<temperatures.size() && temperatures[i]>=temperatures[j])
+           {
+                if (res[j] > 0) j = res[j] + j;
+                else j = temperatures.size();
+           }
+           if(j<temperatures.size())
+               res[i]=j-i;
+       }
+        return res;
     }
 };
