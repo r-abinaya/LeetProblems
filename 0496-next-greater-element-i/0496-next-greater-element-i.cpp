8bc79bb952d4a18f7baace9b3b9e238bd30996ce
R Abinaya <105599764+r-abinaya@users.noreply.github.com>
2023-04-13 22:51:11 +0530

    Time: 10 ms (46.17%), Space: 8.8 MB (65.41%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,31 +1,61 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> result;
-        for(int i=0;i<nums1.size();i++)
+        // vector<int> result;
+        // for(int i=0;i<nums1.size();i++)
+        // {
+        //     int flag=0,flag1=0;
+        //     for(int j=0;j<nums2.size();j++)
+        //     {   
+        //        if(nums1[i]!=nums2[j] && flag==0)
+        //             continue;
+        //         else
+        //         {
+        //             flag=1;
+        //         }
+        //         if(flag==1)
+        //         {
+        //             if(nums1[i]<nums2[j])
+        //             {
+        //                 result.push_back(nums2[j]);
+        //                 flag1=1;
+        //                 break;
+        //             }
+        //         }    
+        //     }
+        //     if(flag1==0)
+        //         result.push_back(-1);
+        // }
+        // return result;
+        vector<int> result(nums1.size(),-1);
+        stack<int> st;
+        unordered_map<int,int> umap;
+        for(int i=0;i<nums2.size();i++)
         {
-            int flag=0,flag1=0;
-            for(int j=0;j<nums2.size();j++)
-            {   
-               if(nums1[i]!=nums2[j] && flag==0)
-                    continue;
-                else
-                {
-                    flag=1;
-                }
-                if(flag==1)
-                {
-                    if(nums1[i]<nums2[j])
-                    {
-                        result.push_back(nums2[j]);
-                        flag1=1;
-                        break;
-                    }
-                }    
+            int element=nums2[i];
+            while(!st.empty() && element>st.top() )
+            {
+                umap[st.top()]=element;
+                st.pop();
             }
-            if(flag1==0)
-                result.push_back(-1);
+            st.push(element);
+        }
+         for(int i=0;i<nums1.size();i++)
+        {
+            int element=nums1[i];
+            if(umap.find(element)!=umap.end())
+            {
+                  result[i]=umap[element];               
+            }
+           
         }
         return result;
+        
     }
+    
 };
+
+
+        
+       
+    
