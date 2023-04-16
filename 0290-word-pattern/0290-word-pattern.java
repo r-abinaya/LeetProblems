d7bde32ae457727be7a24eb52a99a44af37e25de
R Abinaya <105599764+r-abinaya@users.noreply.github.com>
2023-04-16 11:01:01 +0530

    Time: 1 ms (71.71%), Space: 40.7 MB (26.02%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+       String[] words=s.split(" ");
+        if(pattern.length()!=words.length)
+            return false;
+       Map mp=new HashMap();
+       for(Integer i=0;i<words.length;++i)
+           if(mp.put(pattern.charAt(i),i)!=mp.put(words[i],i))
+               return false;
+           return true;
+    }
+}
+
+ // String[] words = str.split(" ");
+ //    if (words.length != pattern.length())
+ //        return false;
+ //    Map index = new HashMap();
+ //    for (Integer i=0; i<words.length; ++i)
+ //        if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
+ //            return false;
+ //    return true;
