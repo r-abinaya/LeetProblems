3aa2a4ad2f3924131c48d88d2be029a74ec0cfb0
R Abinaya <105599764+r-abinaya@users.noreply.github.com>
2023-05-14 11:51:40 +0530

    Time: 2 ms (40.19%), Space: 6.8 MB (52.71%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,82 +1,48 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-//         int m=matrix.size();
-//         int n=matrix[0].size();
-//         int top=0,right=n-1,left=0,bottom=m-1;
-//         vector<int> ans;
-//         int direction=1;
-//         while(left<=right && top<=bottom)
-//         {
-//             if(direction==1)
-//             {
-//                 for(int i=left;i<=right;i++)
-//                     ans.push_back(matrix[top][i]);
-//                 direction=2;
-//                 top++;
-//             }
-//              if(direction==2)
-//             {
-//                 for(int i=top;i<=bottom;i++)
-//                     ans.push_back(matrix[i][right]);
-//                 direction=3;
-//                 right--;
-//             }
-//              if(direction==3)
-//             {
-//                 for(int i=right;i>=left;i--)
-//                     ans.push_back(matrix[bottom][i]);
-//                 direction=4;
-//                 bottom--;
-//             }
-//              if(direction==4)
-//             {
-//                 for(int i=bottom;i>=top;i--)
-//                     ans.push_back(matrix[i][left]);
-//                 direction=1;
-//                 left++;
-//             }
-            
-//         }
-//         return ans;
-        
-        
- int n=matrix.size();
-        int m=matrix[0].size();
-        int left=0,right=m-1,bottom=n-1,top=0;
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int top=0,right=n-1,left=0,bottom=m-1;
+        vector<int> ans;
         int direction=1;
-        vector<int> v;
         while(left<=right && top<=bottom)
         {
             if(direction==1)
             {
-                for(int i=left;i<=right;i++) v.push_back(matrix[top][i]);
+                for(int i=left;i<=right;i++)
+                    ans.push_back(matrix[top][i]);
                 direction=2;
                 top++;
             }
-            
             else if(direction==2)
             {
-                for(int i=top;i<=bottom;i++) v.push_back(matrix[i][right]);
+                for(int i=top;i<=bottom;i++)
+                    ans.push_back(matrix[i][right]);
                 direction=3;
                 right--;
             }
-            
             else if(direction==3)
             {
-                for(int i=right;i>=left;i--) v.push_back(matrix[bottom][i]);
+                for(int i=right;i>=left;i--)
+                    ans.push_back(matrix[bottom][i]);
                 direction=4;
                 bottom--;
             }
-            
             else if(direction==4)
             {
-                for(int i=bottom;i>=top;i--) v.push_back(matrix[i][left]);
+                for(int i=bottom;i>=top;i--)
+                    ans.push_back(matrix[i][left]);
                 direction=1;
                 left++;
             }
+            
         }
-        return v;
+        return ans;
+
+            
+         
+            
     }
 };
 
