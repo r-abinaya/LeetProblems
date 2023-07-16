fef9b8072fc6365c2210aa34ad376cdb0d204dfb
R Abinaya <105599764+r-abinaya@users.noreply.github.com>
2023-07-16 10:23:45 +0530

    Time: 30 ms (14.02%), Space: 19.7 MB (12.46%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-       int key=0;
-        int element=0;
-        int n=nums.size();
-        map<int,int>m;
-        for(int i=0;i<n;i++)
-        {
-            m[nums[i]]++;
-        }
-        for(auto i=m.begin();i!=m.end();i++)
-        {
-            if(i->second>key)
-            {
-                key=i->second;
-                element=i->first;
-             }
-        }
-        return element;
+        sort(nums.begin(),nums.end());
+        return nums[nums.size()/2];
+       // int key=0;
+       //  int element=0;
+       //  int n=nums.size();
+       //  map<int,int>m;
+       //  for(int i=0;i<n;i++)
+       //  {
+       //      m[nums[i]]++;
+       //  }
+       //  for(auto i=m.begin();i!=m.end();i++)
+       //  {
+       //      if(i->second>key)
+       //      {
+       //          key=i->second;
+       //          element=i->first;
+       //       }
+       //  }
+       //  return element;
     }
 };
 
