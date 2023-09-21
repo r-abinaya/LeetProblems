02e2b17b67955cee8ccd956bf5fffb4ff1fefae5
R Abinaya <105599764+r-abinaya@users.noreply.github.com>
2023-09-21 10:24:38 +0530

    Time: 795 ms (17.40%), Space: 22.9 MB (31.88%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,33 +1,41 @@
 class Solution {
-public:
-    string longestPalindrome(string s) {
-           if(s.size()==0) return "";
-        int i = 0, j = 0;
-        int n = s.size();
-        //initialize P[n][n], we only need half of P, and initialize it like: (e.g. : s="abbc")
-        bool P[1000][1000]={false};
-        for(int x = 0;x<n;x++){
-            P[x][x]=true;
-            if(x==n-1) break;
-            P[x][x+1] = (s[x]==s[x+1]);
+
+        private: 
+    bool solve(vector<vector<bool>> &dp, int i, int j, string &s){
+        if(i == j){
+            return dp[i][j] = true;
         }
-        //dp 
-        for(int i = n-3; i>=0; --i){
-            for(int j = i+2;j<n; ++j){
-                P[i][j] = (P[i+1][j-1] && s[i]==s[j]);     
+        if(j-i == 1){
+            if(s[i] == s[j]){
+                return dp[i][j] = true;
+            }
+            else{
+                return dp[i][j] = false;
             }
         }
-        //get maxstr result
-        int max = 0;
-        string maxstr = "";
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(P[i][j]==true and j-i+1>max){
-                    max = j-i+1;
-                    maxstr = s.substr(i,j-i+1);
+        if(s[i] == s[j] && dp[i+1][j-1] == true){
+            return dp[i][j] = true;
+        } else {
+            return dp[i][j] = false;
+        }
+    }
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        int startIndex = 0; int maxlen = 0;
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        for(int g=0; g<n; g++){
+            for(int i=0, j=g; j<n; i++, j++){
+                solve(dp, i, j, s);
+                if(dp[i][j] == true){
+                    if(j-i+1 > maxlen){
+                        startIndex = i;
+                        maxlen = j-i+1;
+                    }
                 }
             }
         }
-        return maxstr;
+        return s.substr(startIndex, maxlen);
     }
+
 };
