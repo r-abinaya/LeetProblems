cf1324f3fa412e9203a51ad22662b69b59035218
R Abinaya <105599764+r-abinaya@users.noreply.github.com>
2023-10-31 15:57:55 +0530

    Time: 154 ms (16.80%), Space: 66.9 MB (37.16%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-   int getBouq(vector<int>& bloomDay,int day,int k)	//Get the number of bouquets of size k at a certain day.
+   int getBouq(vector<int>& bloomDay,int day,int k)
     {
         int result=0,count=0;
         for(int i=0;i<bloomDay.size();i++)
@@ -9,7 +9,7 @@ public:
                 count++;
             else
                 count=0;
-            if(count==k)    //Reset the window size to 0 to consider the case with 2*k or more continous bloomed flowers.
+            if(count==k)
                 result++,count=0;
         }
         return result;
@@ -19,7 +19,7 @@ public:
         auto p=minmax_element(bloomDay.begin(),bloomDay.end());
         int j=*p.second,i=*p.first;
         int mid;
-        while(i<j)				//Binary search on the range to get the minimum day where we may get `m` bouquets.
+        while(i<j)		
         {
             mid=(i+j)>>1;
             if(getBouq(bloomDay,mid,k)<m)
@@ -27,6 +27,6 @@ public:
             else
                 j=mid;
         }
-        return getBouq(bloomDay,i,k)>=m?i:-1;		//Final check if the result day can have atleast `m` bouquets.
+        return getBouq(bloomDay,i,k)>=m?i:-1;		
     }
 };
