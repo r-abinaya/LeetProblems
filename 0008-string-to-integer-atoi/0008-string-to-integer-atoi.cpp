f825c7cd5f0a9791d065e52ee0bba5cd269e8c58
R Abinaya <105599764+r-abinaya@users.noreply.github.com>
2023-10-31 17:04:51 +0530

    Time: 7 ms (5.95%), Space: 6.7 MB (14.05%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int len = s.size();
-        double num = 0;
         int i=0;
-        while(s[i] == ' '){
-            i++;
-        }
-        bool positive = s[i] == '+';
-        bool negative = s[i] == '-';
-        positive == true ? i++ : i;
-        negative == true ? i++ : i;
-        while(i < len && s[i] >= '0' && s[i] <= '9'){
-            num = num*10 + (s[i]-'0');
-            i++;
-        }
-        num = negative ? -num : num;
+        double num=0;
+        int len=s.length();
+        while(s[i]==' ')  i++;
+       bool pos= s[i]=='+' ;
+       bool neg= s[i]=='-' ;
+       pos == true ? i++ : i;
+       neg == true ? i++ : i;
+       while(s[i]>='0' && s[i]<='9' && i<len)
+       {
+           num=(num*10)+(s[i]-'0');
+           i++;
+       }
+        num = neg ? -num : num;
         cout<<num<<endl;
         num = (num > INT_MAX) ? INT_MAX : num;
         num = (num < INT_MIN) ? INT_MIN : num;
         cout<<num<<endl;
         return int(num);
+           
     }
 };
